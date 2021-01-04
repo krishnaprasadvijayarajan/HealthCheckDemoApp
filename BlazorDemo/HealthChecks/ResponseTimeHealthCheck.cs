39d567dc0a3153e28a341ebb0c42b0d8b4bdccc8
Krishna Prasad Vijayarajan <krishnaprasadvijayarajan@users.noreply.github.com>
2021-01-04 20:32:39 +0000

    Add project files.

---
+++ BlazorDemo/HealthChecks/ResponseTimeHealthCheck.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlazorDemo.HealthChecks
+{
+    public class ResponseTimeHealthCheck : IHealthCheck
+    {
+        // Core 5 feature - shorthand initialization of Known Class objects
+        private Random rnd = new();
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            int responseTimeInMS = rnd.Next(1, 300);
+            // Core 5 feature - Logical and Relational switch 
+            return responseTimeInMS switch
+            {
+                <100 => Task.FromResult(HealthCheckResult.Healthy($"The response time looks good ({responseTimeInMS}) ms")),
+                >=100 and <200 => Task.FromResult(HealthCheckResult.Degraded($"The response time is a bit slow ({responseTimeInMS}) ms")),
+                >=200 => Task.FromResult(HealthCheckResult.Unhealthy($"The response time is unacceptable ({responseTimeInMS}) ms"))
+            };
+        }
+    }
+}
---
+++ HealthChecker/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+// https://localhost:44373/healthchecks-ui - to browse the healthchecks page, you the healthchecks-ui as page name
+namespace HealthChecker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
